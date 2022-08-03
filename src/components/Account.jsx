7d65948176d6c7b6b9d505c0c972dd84a3c591a5
StuartCowley <voidstalker@live.co.uk>
2022-08-03 16:07:33 +0100

    Populate details in my account from context

--- src/components/Account.jsx
+++ src/components/Account.jsx
@@ -2,7 +2,11 @@ import "../styles/account.css"
 import Header from "./Header"
 import { useNavigate } from "react-router-dom"
 
+import { useContext } from "react"
+import AuthContext from "../utils/AuthContext"
+
 const Account = () => {
+    const { user } = useContext(AuthContext);
     const navigate = useNavigate()
 
     const handleClick = () => {
@@ -14,11 +18,11 @@ const Account = () => {
             <Header />
             <h1 className="center">My Account</h1>
             <div className="container">
-                <p>Welcome username!</p>
+                <p>Welcome {user.username}!</p>
                 <p>Here are your current details</p>
                 <ul>
-                    <li className="account__user-item">username: <b>yourUsername</b></li>
-                    <li className="account__user-item">email: <b>yourEmail</b></li>
+                    <li className="account__user-item">username: <b>{user.username}</b></li>
+                    <li className="account__user-item">email: <b>{user.email}</b></li>
                 </ul>
                 <button className="account__button" onClick={handleClick}>Logout</button>
             </div>
